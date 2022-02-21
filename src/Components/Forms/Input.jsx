89990eb0d4a21295b746b0abb8d405a84a52f275
Micael Batista <micaelbatista0108@hotmail.com>
2022-02-21 19:46:31 -0300

    second commit

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import styles from './Input.module.css'
+import React from "react";
+import styles from "./Input.module.css";
 
-const Input = ({label, type, name, value, onChange}) => {
+const Input = ({ label, type, name, value, onChange, onBlur, error }) => {
   return (
     <div className={styles.wrapper}>
-      <label htmlFor={name} className={styles.label}>{label}</label>
-      <input id={name} type={type} name={name} className={styles.input} value={value} onChange={onChange} />
-      <p className={styles.error}>Error</p>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        className={styles.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
+  );
+};
 
-  )
-}
-
-export default Input
+export default Input;
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Input from '../Forms/Input'
-import Button from '../Forms/Button'
-import useForm from '../../Hooks/useForm'
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import {UserContext} from '../../UserContext'
 
 const LoginForm = () => {
-
   const username = useForm();
   const password = useForm();
-  console.log(username.value)
+  //console.log(username.value)
 
-  function handleSubmit(event) {
+  const { userLogin } = React.useContext(UserContext);
+
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify() 
-    })
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-    })
-  }
 
+    if (username.validate() && password.validate()) {
+
+      userLogin(username.value, password.value);
+
+    }
+  }
 
   return (
     <section>
@@ -36,11 +30,10 @@ const LoginForm = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
-
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
