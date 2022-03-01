db14e2fd156bf862c453222e1a4dfc1b83699ef0
Micael Batista <micaelbatista0108@hotmail.com>
2022-03-01 14:54:13 -0300

    finishing the project

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -20,6 +23,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="login/*" element={<Login />} />
         <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
+        <Route path="foto/:id" element={<Photo />} />
+        <Route path="perfil/:user" element={<UserProfile />} />
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
       <Footer />
       </UserStorage>
